528b260439f8e8a32e2580d4078d799c5b6cf42e
ROTNdev <rotn.edm@gmail.com>
2014-09-19 12:19:48 +1000

    Reset

--- CPlusPlusPlayground/CPlusPlusPlayground/CPlusPlusPlayground.cpp
+++ CPlusPlusPlayground/CPlusPlusPlayground/CPlusPlusPlayground.cpp
@@ -10,17 +10,6 @@
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[]) {
-    int max = 0;
-    int list[100];
-    srand(time(0));
-    for (int i = 0; i < 100; i++) {
-        list[i] = rand();
-    }
-    for (int i = 0; i < 100; i++) {
-        cout << "heh:\t " << list[i] << endl;
-        if (max < list[i]) max = list[i];
-    }
-    cout << "The max number is:\t" << max << endl;
     system("pause");
     return 0;
 }
