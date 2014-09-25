21fe0e2cca3a2aca36520a36ba141e87a9b3b8c0
ROTNdev <rotn.edm@gmail.com>
2014-09-25 11:57:09 +1000

    Reset

--- CPlusPlusPlayground/CPlusPlusPlayground/CPlusPlusPlayground.cpp
+++ CPlusPlusPlayground/CPlusPlusPlayground/CPlusPlusPlayground.cpp
@@ -3,25 +3,12 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <ctime>
-#include <math.h>
 #include <string>
 #include <windows.h>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[]) {
-    string foo;
-    cin >> foo;
-    double result = 0;
-    for (int i = 0; i < 8; i++) {
-        cout << i << "\t" << foo[i] << endl;
-        if (foo[i] == '1') {
-            result += pow(2, 7 - i);
-            cout << "Result:\n" << result << endl;
-        }
-    }
-    cout << result;
     system("pause");
     return 0;
 }
